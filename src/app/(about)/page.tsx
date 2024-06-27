af9adefcce856fea62173815c6ee8f0304c6d721
Hardik Dhuri <49452742+HardikDhuri@users.noreply.github.com>
2024-06-27 14:14:54 +0000

    engg: Configure routing for nav items

---
+++ src/app/(about)/page.tsx
@@ -0,0 +1,14 @@
+export default function Page() {
+  return (
+    <>
+      <div className="flex">
+        <div className="bg-slate-500 flex-1 h-screen">
+          <h1 className="text-white text-4xl text-center">Part 1</h1>
+        </div>
+        <div className="bg-red-500 flex-1 h-screen">
+          <h2 className="text-white text-4xl text-center">Part 2</h2>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/app/connect/page.tsx
@@ -0,0 +1,14 @@
+export default function Page() {
+  return (
+    <>
+      <div className="flex">
+        <div className="bg-pink-500 flex-1 h-screen">
+          <h1 className="text-white text-4xl text-center">Connect</h1>
+        </div>
+        <div className="bg-red-500 flex-1 h-screen">
+          <h2 className="text-white text-4xl text-center">Part 2</h2>
+        </div>
+      </div>
+    </>
+  );
+}
